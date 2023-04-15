c6794538debdcc343ef21f65e80bb56c8936cc8a
de_brasi <ya.kakadu.ru@yandex.ru>
2023-04-15 22:54:37 +0300

    functions declaration

--- utils/core_api.py
+++ utils/core_api.py
@@ -1,1 +1,25 @@
 import config
+
+
+def add_phot():
+    pass
+
+
+def delete_photo():
+    pass
+
+
+def get_added_photos():
+    pass
+
+
+def add_time():
+    pass
+
+
+def set_default_time():
+    pass
+
+
+def get_stored_time():
+    pass
