9ede633796638509649cd6deac49cb0debce4ded
de_brasi <ya.kakadu.ru@yandex.ru>
2023-04-17 15:40:41 +0300

    add todo for console-only running

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from utils.gui_maker import make_gui
 
 if __name__ == "__main__":
+
+    # TODO:
+    #  для минимального использования необходимо запускать без GUI,
+    #  то есть нужен аргумент из консоли с типом запуска
+    #  (как консольное приложение, либо с интерфейсом)
+
     # init context: interface and stored with previous run data
     make_gui()
 
