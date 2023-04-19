57bc6c2fdc9013ae89dcda13ef7294341f271a44
de_brasi <ya.kakadu.ru@yandex.ru>
2023-04-19 10:52:06 +0300

    using_vk_api draft

--- utils/using_vk_api.py
+++ utils/using_vk_api.py
@@ -1,1 +1,12 @@
 # TODO: все функции связанные с запросами к серверам ВК - сюда
+
+VkSession = int     # todo: session type from vk
+
+
+def create_session(login, password) -> VkSession:
+    # создать сессию ВК (для одного пользователя по логину и паролю)
+    return 0
+
+
+def start_with_delay(session, queries_arguments, time):
+    pass
