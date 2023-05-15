2de4777c0459aaf2557861a0671a7619ec775d11
de-brasi <leskin.ilia.public@yandex.ru>
2023-05-15 21:43:38 +0300

    window appearance

--- utils/gui_maker.py
+++ utils/gui_maker.py
@@ -1,10 +1,29 @@
 # TODO: всю отрисовку интерфейса в этот модуль
+
+import sys
+import pathlib
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon
 from main_standalone_window import Ui_MainWindow
 
 import utils.core_api
 
+# this fix problem with relative path after importing module
+current_file_dir_path = str(pathlib.Path(__file__).parent)
+current_file_dir_parent_path = str(pathlib.Path(__file__).parent.parent)
 
-class MainWindow:
-    pass
+
+class Example(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('АвтоКоммент')
+        # Icon source:
+        # <a href="https://www.flaticon.com/ru/free-icons/" title="робот иконки">Робот иконки от Freepik - Flaticon</a>
+        self.setWindowIcon(QIcon(current_file_dir_parent_path + '/static/main_window_icon.png'))
+        self.ui.add_link_entry.setPlaceholderText('Введите ссылку добавляемого объекта')
+        self.ui.delete_link_entry.setPlaceholderText('Введите ссылку удаляемого объекта')
