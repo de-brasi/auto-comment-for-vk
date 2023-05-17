fccf185e0ef398f1d85327a771e4e424264655ef
de-brasi <leskin.ilia.public@yandex.ru>
2023-05-17 14:40:43 +0300

    prettify

--- utils/core_api.py
+++ utils/core_api.py
@@ -12,10 +12,12 @@ import json
 import sys
 import pathlib
 
+import config
+
+# Warning! Path of directory that contains a path need to be added to PATH before importing using_vk_api!
 parents_path = str(pathlib.Path(__file__).parent)
 sys.path.append(parents_path)
 
-import config
 import using_vk_api
 
 
