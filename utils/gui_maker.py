f070a878f41acb30464bfb165e7e68136aab8b31
de-brasi <leskin.ilia.public@yandex.ru>
2023-05-17 14:10:56 +0300

    add button functions

--- utils/gui_maker.py
+++ utils/gui_maker.py
@@ -1,12 +1,11 @@
 # TODO: всю отрисовку интерфейса в этот модуль
 
-import sys
 import pathlib
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon
 from main_standalone_window import Ui_MainWindow
 
-import utils.core_api
+import utils.core_api as core_api
 
 # this fix problem with relative path after importing module
 current_file_dir_path = str(pathlib.Path(__file__).parent)
@@ -27,3 +26,30 @@ class Interface(QtWidgets.QMainWindow):
         self.setWindowIcon(QIcon(current_file_dir_parent_path + '/static/main_window_icon.png'))
         self.ui.add_link_entry.setPlaceholderText('Введите ссылку добавляемого объекта')
         self.ui.delete_link_entry.setPlaceholderText('Введите ссылку удаляемого объекта')
+
+        # buttons
+        self.ui.add_link_entry_button.clicked.connect(self.get_link_to_add)
+        self.ui.delete_link_entry_button.clicked.connect(self.get_link_to_delete)
+
+    def get_link_to_add(self):
+        entry_link_value = self.ui.add_link_entry.text()
+        self.ui.add_link_entry.clear()
+        core_api.add_photo(entry_link_value)
+
+        # todo: debug
+        print(core_api.get_added_photos())
+
+    def get_link_to_delete(self):
+        # todo: выводить окно с сообщением удалено или нет
+        entry_link_value = self.ui.delete_link_entry.text()
+        self.ui.delete_link_entry.clear()
+        core_api.delete_photo(entry_link_value)
+
+        # todo: debug
+        print(core_api.get_added_photos())
+
+    def get_time(self):
+        pass
+
+    def start_execution(self):
+        pass
